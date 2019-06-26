00debabc2109f555f5bc52bc8561dd7b0daa90e0
Tolulope Agabielesin <Tagabielesin@wragbysolutions.com>
2019-06-26 09:25:24 +0100

    Add project files.

---
+++ UDDO/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UUBO.Models;
+
+namespace UUBO.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ UDDO/Models/ApplicationRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UUBO.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole(string roleName) : base(roleName)
+        {
+        }
+        public ApplicationRole()
+        {
+
+        }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string IPAddress { get; set; }
+
+    }
+}
